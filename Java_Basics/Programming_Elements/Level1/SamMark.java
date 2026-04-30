326922ea823a690e61924122efa9b95d793d7887
ArpitGupta0212 <arpitboss52525@gmail.com>
2026-04-30 11:27:32 +0530

    [Arpit] Sam marks problem completed

---
+++ Java_Basics/Programming_Elements/Level1/SamMark.java
@@ -0,0 +1,8 @@
+public class SamMark {
+    public static void main(String args[]){
+        int phy=95;
+        int maths=94;
+        int chem=96;
+        System.out.println((phy+chem+maths)/3);
+    }
+}
