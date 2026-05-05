f2b907d332246d7214c912660970244e50c2981d
ArpitGupta0212 <arpitboss52525@gmail.com>
2026-05-05 12:12:07 +0530

    [Arpit] Smallest Number Check Program Completed

---
+++ Java_Basics/Control_Flow/Level1/SmallestCheck.java
@@ -0,0 +1,14 @@
+public class SmallestCheck {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        if (num1 < num2 && num1 < num3) {
+            System.out.println("Is the first number the smallest? Yes");
+        } else {
+            System.out.println("Is the first number the smallest? No");
+        }
+    }
+}
