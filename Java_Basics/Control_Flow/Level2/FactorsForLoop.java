dc3bebb279fbd5e07ccccbd625b524e134a957cd
ArpitGupta0212 <arpitboss52525@gmail.com>
2026-05-06 12:26:42 +0530

    [Arpit] Factors For Loop Program Completed

---
+++ Java_Basics/Control_Flow/Level2/FactorsForLoop.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class FactorsForLoop {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+
+            System.out.println("Factors are:");
+
+            for (int i = 1; i < number; i++) {
+
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+
+            }
+
+        } else {
+            System.out.println("Not a Positive Integer");
+        }
+
+        sc.close();
+    }
+}
