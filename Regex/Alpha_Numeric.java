848862c6380074a40142155c21903c17f634d552
ArpitGupta0212 <arpitboss52525@gmail.com>
2026-06-02 16:14:56 +0530

    [Arpit Gupta] Alpha Numeric Program Completed

--- Regex/Alpha_Numeric.java
+++ Regex/Alpha_Numeric.java
@@ -3,9 +3,9 @@ import java.util.regex.*;
 
 public class Alpha_Numeric {
     public static void main(String args[]){
-        String str="a*b";
+        String str="[a-zA-Z0-9#]*";
         Pattern pt=Pattern.compile(str);
-        Matcher mt=pt.matcher("acb");
+        Matcher mt=pt.matcher("68#47C");
         // Matches is used for exact same
         System.out.println(mt.matches());
     }
