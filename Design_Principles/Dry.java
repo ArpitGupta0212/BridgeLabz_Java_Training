87d5c3fc49938e5376b1046967112e518acede2b
ArpitGupta0212 <arpitboss52525@gmail.com>
2026-06-08 14:47:10 +0530

    [Arpit Gupta] Dry program completed

--- Design_Principles/Dry.java
+++ Design_Principles/Dry.java
@@ -1,3 +1,9 @@
 public class Dry{
-    
+    public static void print(){
+        System.out.println("Do Not repeat yourself");
+    }
+    public static void main(String[] args) {
+        print();
+        print();
+    }
 }
