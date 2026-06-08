41e754b1b9f8e5c4e69a5ab4b2504c5af9bb2b81
ArpitGupta0212 <arpitboss52525@gmail.com>
2026-06-08 14:56:36 +0530

    [Arpit Gupta] Kiss program completed

---
+++ Design_Principles/Kiss.java
@@ -0,0 +1,11 @@
+public class Kiss {
+    public static void main(String[] args) {
+        int age=19;
+        if(age>=18){
+            System.out.println("This is kiss example");
+        }
+        if((age>=18)==true){
+            System.out.println("This is Not kiss example");
+        }
+    }
+}
