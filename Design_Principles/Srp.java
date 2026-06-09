625abab29f785ee4c0676df0c1e5a91f640b7918
ArpitGupta0212 <arpitboss52525@gmail.com>
2026-06-09 11:20:54 +0530

    [Arpit] Srp program completed

---
+++ Design_Principles/Srp.java
@@ -0,0 +1,12 @@
+class StudentService{
+    public void studentinfo(){
+        System.out.println("Info about Student");
+    }
+} 
+class ReportStudent{
+    public void studentReport(){
+        System.out.println("Report of Students");
+    }
+}
+public class Srp {
+}
