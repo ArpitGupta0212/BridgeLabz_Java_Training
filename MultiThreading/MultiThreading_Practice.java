3de4ce59c9f90c529cf8d6c50a06ec1c609dd946
ArpitGupta0212 <arpitboss52525@gmail.com>
2026-06-16 14:59:56 +0530

    [Arpit Gupta] MultiThread Using class Program Completed'

--- MultiThreading/MultiThreading_Practice.java
+++
@@ -1,5 +0,0 @@
-public class MultiThreading_Practice{
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ MultiThreading/MultiThreading_Practice_Using_Thread_ClaSS.java
@@ -0,0 +1,12 @@
+public class MultiThreading_Practice_Using_Thread_ClaSS{    
+    public static void main(String[] args) {
+        MyThread t1=new MyThread();
+        t1.start();
+        
+    }
+}
+class MyThread extends Thread{
+        public void run(){
+            System.out.println("Your Thread Is Running");
+        }
+    }
