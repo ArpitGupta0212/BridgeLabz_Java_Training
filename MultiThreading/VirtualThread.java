693e14793ea3826b419befb3286a4e4dc68c01cc
ArpitGupta0212 <arpitboss52525@gmail.com>
2026-06-17 11:47:01 +0530

    [Arpit Gupta] Virtula Thread Program Completed

---
+++ MultiThreading/VirtualThread.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class VirtualThread{
+    public static void main(String args[]){
+        ExecutorService ex=Executors.newVirtualThreadPerTaskExecutor();
+        for(int i=0;i<10;i++){
+            ex.submit(()->{
+                System.out.println(Thread.currentThread());
+            });
+        }
+        try{
+            Thread.sleep(3000);
+        }
+        catch(Exception e){}
+    }
+}
