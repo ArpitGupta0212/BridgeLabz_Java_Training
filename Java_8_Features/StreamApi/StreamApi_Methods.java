7456ced7c56a35e64296d3d23add20ea7c3002dd
ArpitGupta0212 <arpitboss52525@gmail.com>
2026-06-17 16:01:15 +0530

    [Arpit Gupta] Stream Api Method Program Completed

--- Java_8_Features/StreamApi/StreamApi_Methods.java
+++ Java_8_Features/StreamApi/StreamApi_Methods.java
@@ -1,5 +1,30 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StreamApi_Methods {
     public static void main(String[] args) {
-        
+        // filter(Predicate)
+        // predicate take boolean value function
+        List<String>li=List.of("Aman", "Durgesh","GhanShyam");
+        List<String> li1=li.stream().filter((e)->{
+            if(e.charAt(0)=='A'){
+                return true;
+            }else{
+                return false;
+            }
+        }).collect(Collectors.toList());
+        System.out.println(li1);
+        // map (FUnction)
+        // each emenet operation
+        List<Integer> listNum=List.of(1,3,4,5,5,6,7,7,74,3,32,5);
+        List<Integer> ans=listNum.stream().map(e->{
+            return e*e;
+        }).collect(Collectors.toList());
+        System.out.println(ans);
+        listNum.stream().sorted().forEach(System.out::println);
+        int min=listNum.stream().min((x,y)->x.compareTo(y)).get();
+        System.out.print(min);
+        int max=listNum.stream().max((x,y)->x.compareTo(y)).get();
+        System.out.print(max);
     }
 }
